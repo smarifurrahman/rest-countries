8637c2ac5a3c366f792f25e039af856d78bed9fc
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-03-20 09:33:49 +0600

    option 2 - destructuring in parameters

--- js/countries.js
+++ js/countries.js
@@ -10,9 +10,8 @@ const displayCountries = countries => {
     countriesContainer.innerHTML = countriesHTML.join(' ');
 }
 
-// option 1
-const getCountryHTML = country => {
-    const {name, flags, capital} = country;
+// option 2
+const getCountryHTML = ({name, flags, capital}) => {
     return `
     <div class="country">
         <h3>${name.common}</h3>
@@ -22,7 +21,19 @@ const getCountryHTML = country => {
     `;
 }
 
-// original
+// // option 1
+// const getCountryHTML = country => {
+//     const {name, flags, capital} = country;
+//     return `
+//     <div class="country">
+//         <h3>${name.common}</h3>
+//         <img src="${flags.png}">
+//         <h3>Capital: ${capital ? capital : 'Not Found'}</h3>
+//     </div>
+//     `;
+// }
+
+// // original
 // const getCountryHTML = country => {
 //     return `
 //     <div class="country">
